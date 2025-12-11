99a13aa97d3475f50ab2b96efba024c40cc33821
Mohammad-Rashid007 <you@example.com>
2025-12-11 19:31:36 +0530

    Initial commit

---
+++ customerapp/customerappurls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('customerhome/',views.customerhome,name="customerhome"),
+    path('logout/',views.logout,name="logout"),
+    path('response/',views.response,name="response"),
+    path('viewprofile/',views.viewprofile,name="viewprofile"),
+    path('products/',views.products,name="products"),
+    path('changepasswords/',views.changepasswords,name="changepasswords"),
+]
